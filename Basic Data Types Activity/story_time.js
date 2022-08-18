0e9d2c8faa8b5c6d9e5b99bd48039ddbe49733f4
amycluo54 <shilgenetics@gmail.com>
2022-08-18 10:08:58 -0400

    .

---
+++ Basic Data Types Activity/story_time.js
@@ -0,0 +1,33 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+let welcome = "Hello Traveler";
+let name = "Hermes";
+let setting = "The Underworld";
+let river = "the River Styx";
+
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+let age = "28";
+let year = "1980";
+let born = "1952";
+
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+
+let places = ['Tartarus', "The Fields of Mourning", "The Asphodel Meadows", "The Elysian Fields"];
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+
+let judgesRule = true;
+
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+console.log(welcome + ", I'm your guide, " + name + "!");
+console.log("Welcome to " + setting + ".")
+console.log("I'll take y'all to " + river + " for Charon to ferry over.")
+console.log("\n Timeskip: Judge's Pavilion")
+console.log("Judge Minos: year born - "+ born+ ", year died - "+year+", age at death - "+age+ ".")
+console.log("Judge Aeacus: Based on your mortal deed's we have ruled for you to end up in " + places[2]+".")
+console.log("Judge Rhadamanthus: Our "+judgesRule+" understanding may serve you well in the Underworld.")
+console.log("\n Begin your quest: return to the world of living after an injustified death")
